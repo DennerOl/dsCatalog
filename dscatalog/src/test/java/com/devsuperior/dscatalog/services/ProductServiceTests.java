74e4b30e2767d31f61005d7ff7ac2aec1f2820a5
DennerOl <dener155@hotmail.com>
2023-09-05 21:32:30 -0300

    Test Delete Id integridade referencial

--- dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -7,10 +7,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.service.ProductService;
+import com.devsuperior.dscatalog.service.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.service.exceptions.ResourceNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -23,24 +26,49 @@ public class ProductServiceTests {
 	
 	private long existingId;
 	private long nonExistingId;
+	private long dependetId;
 
 // inicia antes de cada metodo	
 	@BeforeEach
 	void setUp() throws Exception{
 		existingId = 1L;
 		nonExistingId = 1000L;
+		dependetId = 3L;
 		
 /* configuração do comportamento simulado do repository
  * quando chamar o deleteById com id existente ele não irá fazer nada
  */	
+// simulo se o id é existente e retorno verdadeiro		
 		Mockito.when(repository.existsById(existingId)).thenReturn(true);
-
 		
 		Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
+
+// mokito id com integridade referencial no banco
+		Mockito.when(repository.existsById(dependetId)).thenReturn(true);
+		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependetId);
+		
+		
+	}
+	
+	
+	@Test
+	public void naoDeleteIdComIntegridadeNoBancoDados() {
+		Assertions.assertThrows(DatabaseException.class, () -> {
+			service.delete(dependetId);
+		});
 	}
 	
 	@Test
-	public void naoFazNadaComIdExistente() {
+	public void deleteDeveriaResourceNotFoundExceptionIdNaoExistente() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, ()->{
+			service.delete(nonExistingId);
+		});
+	}
+	
+	
+	@Test
+	public void deleteNaoFazNadaComIdExistente() {
 		
 // não deveria ter exceção na Assertions		
 		Assertions.assertDoesNotThrow(() -> {
