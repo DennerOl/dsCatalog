e3f224e647886878656dfcd00389f9150a4b7ba0
DennerOl <dener155@hotmail.com>
2023-09-06 21:32:55 -0300

    Test findAll paginado

--- dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ dscatalog/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,19 +1,30 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.service.ProductService;
 import com.devsuperior.dscatalog.service.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.service.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.testes.Factory;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -27,6 +38,10 @@ public class ProductServiceTests {
 	private long existingId;
 	private long nonExistingId;
 	private long dependetId;
+	
+	private PageImpl<Product> page;
+	private Product product;
+	
 
 // inicia antes de cada metodo	
 	@BeforeEach
@@ -34,6 +49,9 @@ public class ProductServiceTests {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		dependetId = 3L;
+		
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
 		
 /* configuração do comportamento simulado do repository
  * quando chamar o deleteById com id existente ele não irá fazer nada
@@ -47,6 +65,27 @@ public class ProductServiceTests {
 		Mockito.when(repository.existsById(dependetId)).thenReturn(true);
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependetId);
 		
+// mockito para findAll paginado 
+	Mockito.when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);	
+	
+/* mockito para Save do repository
+ * quando eu chamar o Save em qualquer obj
+ * retorna um produto
+ */
+	Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+	
+// mockito para findById
+	Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
+	Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+	}
+	
+	@Test public void findAllPageDeveRetornarUmaPagina() {
+		
+		Pageable pageable = PageRequest.of(0,10);
+		Page<ProductDTO> result = service.findAllPaged(pageable);
+		
+		Assertions.assertNotNull(result);
+		Mockito.verify(repository).findAll(pageable);
 		
 	}
 	
