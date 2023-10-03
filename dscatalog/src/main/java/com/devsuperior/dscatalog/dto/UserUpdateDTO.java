98d95c5fc6ca5290fa1a2a81d1aa7336a244b52d
DennerOl <dener155@hotmail.com>
2023-10-02 21:37:50 -0300

    ContraintValidator iniciando validação customizada update

---
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,18 @@
+package com.devsuperior.dscatalog.dto;
+
+import com.devsuperior.dscatalog.service.validation.UserInsertValid;
+import com.devsuperior.dscatalog.service.validation.UserUpdateValid;
+
+/* criado para carregar todos os daddos de um Use
+ * incluso a senha. Sera usado quando for inserido um
+ * novo usuario 
+ */
+
+@SuppressWarnings("serial")
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO{
+
+	
+	
+	
+}
