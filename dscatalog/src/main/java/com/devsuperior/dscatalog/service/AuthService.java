24d947167e01c6c6a0f976e59e72bdec36a9cf9a
DennerOl <dener155@hotmail.com>
2023-11-06 21:13:49 -0300

    Primeiro passo para recuperação de senha

---
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/service/AuthService.java
@@ -0,0 +1,17 @@
+package com.devsuperior.dscatalog.service;
+
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.dto.EmailDTO;
+
+import jakarta.validation.Valid;
+
+@Service
+public class AuthService {
+
+	public void createRecoverToken( EmailDTO body) {
+		
+		
+	}
+
+}
